4b4665d2a87452dc01b782cb4d3145fb86418e0a
1ashish2 <ashishashish0507@gmail.com>
2022-09-04 00:09:34 +0530

    git workflow change33

--- marketing/config/webpack.prod.js
+++ marketing/config/webpack.prod.js
@@ -7,7 +7,8 @@ const packageJson=require('../package.json');
 const prodConfig={
     mode: 'production',
     output: {
-       filename:'[name].[contenthash].js' 
+        filename: '[name].[contenthash].js',
+        publicPath: '/marketing/latest/',
     },
     plugins:[
         new ModuleFederationPlugin({
