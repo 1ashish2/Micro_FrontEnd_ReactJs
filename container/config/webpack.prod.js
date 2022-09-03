08f8ed15ecc60a0de055196c434216fd20a9ad84
1ashish2 <ashishashish0507@gmail.com>
2022-09-03 23:39:14 +0530

    git workflow change30

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -8,7 +8,12 @@ const domain = process.env.PRODUCTION_DOMAIN;
 const prodConfig={
     mode: 'production',
     output: {
-       filename:'[name].[contenthash].js' 
+        filename: '[name].[contenthash].js',
+        publicPath: '/container/latest/',
+        // output will be urlpath/container/latest/filename.contenthash.js -->this will provide becoz in s3
+        // bucket we created container/latest/our all files after deployment so we need to aceess mail file so 
+        // we need that file location path to acces the file and show to url
+        // this setup is done when we got error to load page (unexpected token '<')
     },
     plugins:[
         new ModuleFederationPlugin({
