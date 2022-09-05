b473e5f25752aef949489edd1908d57f5828eff2
1ashish2 <ashishashish0507@gmail.com>
2022-09-05 12:37:56 +0530

    finished feature 2

--- container/src/App.js
+++ container/src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import{mount} from 'marketing/MarketingApp';
 import MarketingApp from './component/MarketingApp'
+import Header from "./component/Header";
 console.log(mount)
 export default ()=>{
     return (
-        <>
-            <h2>WelCome</h2>
-         <MarketingApp />  
-        </>
+        <BrowserRouter>
+            <div>
+                 <Header />
+                <MarketingApp />  
+            </div>
+         
+        </BrowserRouter>
        
     )
 }
