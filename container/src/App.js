309c47a483421be73dcf73412e2d2edcd7fded2a
1ashish2 <ashishashish0507@gmail.com>
2022-09-05 14:08:18 +0530

    css fix for marketing and container

--- container/src/App.js
+++ container/src/App.js
@@ -3,14 +3,23 @@ import { BrowserRouter } from "react-router-dom";
 import{mount} from 'marketing/MarketingApp';
 import MarketingApp from './component/MarketingApp'
 import Header from "./component/Header";
-console.log(mount)
+import {StylesProvider,createGenerateClassName} from '@material-ui/core/styles'
+// console.log(mount)
+const generateClassName = createGenerateClassName({
+    productionPrefix: "co",
+    // setting prefix random with co(name) starting in production mode to make unique
+    // so it will not collide
+})
 export default ()=>{
     return (
         <BrowserRouter>
-            <div>
+            <StylesProvider generateClassName={generateClassName}>
+                <div>
                  <Header />
                 <MarketingApp />  
             </div>
+            </StylesProvider>
+            
          
         </BrowserRouter>
        
--- marketing/src/App.js
+++ marketing/src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import{BrowserRouter,Route,Switch} from 'react-router-dom'
-import {StylesProvider} from '@material-ui/core/styles'
+import {StylesProvider,createGenerateClassName} from '@material-ui/core/styles'
 import Landing from './components/Landing';
-import Pricing  from './components/Pricing'
+import Pricing from './components/Pricing'
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: "ma",
+    // setting prefix random with ma(name) starting in production mode to make unique
+    // so it will not collide
+})
+
 const App=()=>{
     return(
         <div>
-            <StylesProvider>
+            <StylesProvider generateClassName={generateClassName} >
                 <BrowserRouter>
                 <Switch>
                     <Route exact path="/pricing" component={Pricing}/>
