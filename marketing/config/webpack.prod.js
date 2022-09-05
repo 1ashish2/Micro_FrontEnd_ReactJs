4fffef2630046e6e4f1858863f7bb89356251008
1ashish2 <ashishashish0507@gmail.com>
2022-09-05 11:58:00 +0530

    git workflow change40

--- marketing/config/webpack.prod.js
+++ marketing/config/webpack.prod.js
@@ -12,7 +12,7 @@ const prodConfig={
     },
     plugins:[
         new ModuleFederationPlugin({
-            name:'container',
+            name:'marketing',
             filename:'remoteEntry.js',
             exposes:{
                 './MarketingApp':'./src/bootstrap'
