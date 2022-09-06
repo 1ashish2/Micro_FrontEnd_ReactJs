cd723a4d2c6859d0e23e0d456df59e31fec5e3e2
1ashish2 <ashishashish0507@gmail.com>
2022-09-06 14:54:54 +0530

    communication between apps

--- container/src/App.js
+++ container/src/App.js
@@ -1,27 +1,26 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import{mount} from 'marketing/MarketingApp';
-import MarketingApp from './component/MarketingApp'
-import Header from "./component/Header";
-import {StylesProvider,createGenerateClassName} from '@material-ui/core/styles'
-// console.log(mount)
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
+
+import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
+
 const generateClassName = createGenerateClassName({
-    productionPrefix: "co",
-    // setting prefix random with co(name) starting in production mode to make unique
-    // so it will not collide
-})
-export default ()=>{
-    return (
-        <BrowserRouter>
-            <StylesProvider generateClassName={generateClassName}>
-                <div>
-                 <Header />
-                <MarketingApp />  
-            </div>
-            </StylesProvider>
-            
-         
-        </BrowserRouter>
-       
-    )
-}
+  productionPrefix: 'co',
+});
+
+export default () => {
+  return (
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
+  );
+};
--- marketing/src/App.js
+++ marketing/src/App.js
@@ -1,28 +1,28 @@
-import React from "react";
-import{BrowserRouter,Route,Switch} from 'react-router-dom'
-import {StylesProvider,createGenerateClassName} from '@material-ui/core/styles'
+import React from 'react';
+import { Switch, Route, Router } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
+
 import Landing from './components/Landing';
-import Pricing from './components/Pricing'
+import Pricing from './components/Pricing';
 
 const generateClassName = createGenerateClassName({
-    productionPrefix: "ma",
-    // setting prefix random with ma(name) starting in production mode to make unique
-    // so it will not collide
-})
+  productionPrefix: 'ma',
+});
 
-const App=()=>{
-    return(
-        <div>
-            <StylesProvider generateClassName={generateClassName} >
-                <BrowserRouter>
-                <Switch>
-                    <Route exact path="/pricing" component={Pricing}/>
-                    <Route exact path="/" component={Landing}/>
-                </Switch>
-                </BrowserRouter>
-            </StylesProvider>
-        </div>
-    )
-}
-
-export default App;
+export default ({ history }) => {
+  return (
+    <div>
+      <StylesProvider generateClassName={generateClassName}>
+        <Router history={history}>
+          <Switch>
+            <Route exact path="/pricing" component={Pricing} />
+            <Route path="/" component={Landing} />
+          </Switch>
+        </Router>
+      </StylesProvider>
+    </div>
+  );
+};
